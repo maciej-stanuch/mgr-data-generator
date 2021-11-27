099627bb32739cde4821ed77ca05ec7aedaf2f22
Maciej Stanuch <maciejstanuch.ms@gmail.com>
2021-11-27 17:34:45 +0100

    Add functions for inserting into the database

--- src/data_generator/core.clj
+++ src/data_generator/core.clj
@@ -3,15 +3,27 @@
   (:require [clojure.walk :as walk]
             [clojure.set :as set]
             [data-generator.model :as model]
-            [data-generator.date-utils :as date-utils]))
+            [data-generator.date-utils :as date-utils]
+            [data-generator.database.mongo :as mongo]))
 
-(declare cmd-params
-         validate-params)
-
-(def ^:const known-program-params
+(def ^{:const true :private true} known-program-params
   #{:--db-ip
     :--db-port
     :--data-size})
+
+(defn- cmd-params
+  "Parses the command line params to the key value map."
+  [args]
+  (walk/keywordize-keys (apply hash-map args)))
+
+(defn- validate-params
+  "Validates the command line params passed to the program."
+  [params]
+  (if (empty? params)
+    #{"Parameters are required to run the program."}
+    (let [unrecognized-params (set/difference (set (keys params))
+                                              known-program-params)]
+      (map #(str "Unrecognized parameter '" (name %) "'.") unrecognized-params))))
 
 (defn -main
   [& args]
@@ -23,20 +35,11 @@
         (println "Running with parameters:")
         (println params)
         (println (str "Random activity: " (rand-nth model/activities)))
-        (println (str "Now: " (date-utils/now)))
-        (println (str "Next: " (date-utils/random-next-date (date-utils/now)))))
+        (println (str "Now:\t" (date-utils/now)))
+        (println (str "Next:\t" (date-utils/random-next-date (date-utils/now))))
+        (let [conn (mongo/db-connection (:--db-ip params) (:--db-port params))
+              db (mongo/get-db conn "admin")]
+          (mongo/db-insert-document db "session" {:test "test" :abc "abc"})))
       (do
         (println "Encountered errors:")
         (doseq [error errors] (println (str "- " error)))))))
-
-(defn- cmd-params
-  "Parses the command line params to the key value map."
-  [args]
-  (walk/keywordize-keys (apply hash-map args)))
-
-(defn- validate-params
-  "Validates the command line params passed to the program."
-  [params]
-  (map #(str "Unrecognized parameter '" (name %) "'.") (set/difference
-                                                         (set (keys params))
-                                                         known-program-params)))
--- src/data_generator/database/mongo.clj
+++ src/data_generator/database/mongo.clj
@@ -1,11 +1,21 @@
 (ns data-generator.database.mongo
-  (:require [monger.core :as mg]))
+  (:require [monger.core :as mg]
+            [monger.collection :as mc]))
 
 (defn db-connection
   "Returns a monger database connection."
   [db-ip db-port]
   (mg/connect {:host db-ip
                :port (Integer/parseInt db-port)}))
+
+(defn get-db [conn name]
+  (mg/get-db conn name))
+
+(defn db-insert-document [db collection document]
+  (mc/insert-and-return db collection document))
+
+(defn db-insert-document-batch [db collection documents]
+  (mc/insert-batch db collection documents))
 
 (defn db-connection-close
   "Closes the database connection."
